4ade548619ac4fdc0e441f1c80f271d4c8cea222
nagaraj rathna <34264078+nagaraj28@users.noreply.github.com>
2021-12-04 19:38:16 +0530

    feat: brands and categories routes

--- routes/products.js
+++ routes/products.js
@@ -24,6 +24,51 @@ router.route("/").get(async(req,res)=>{
 }
 );
 
+/* 
+fetch  all the brands
+*/
+router.route("/brands").get(async(req,res)=>{
+    try{
+        const brands =await products.distinct("brand");
+        if(brands)
+        res.status(200).json({
+            status:"success",
+            data:brands
+        });
+        else
+        throw(brands)
+    }
+    catch(err){
+        res.status(404).json({
+            status:"fail",
+            errorMessage:err
+        })
+    }
+})
+
+/* 
+fetch  all the categories
+*/
+router.route("/categories").get(async(req,res)=>{
+    try{
+        const categories =await products.distinct("category");
+        if(categories)
+        res.status(200).json({
+            status:"success",
+            data:categories
+        });
+        else
+        throw(categories)
+    }
+    catch(err){
+        res.status(404).json({
+            status:"fail",
+            errorMessage:err
+        })
+    }
+})
+
+
 /*
 add  the products into  database
 */
